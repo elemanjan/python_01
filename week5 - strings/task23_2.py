bc6f108058aa7dc6990e9314fb714dd1315418c3
elemanjan <mr.eleman@gmail.com>
2020-12-09 19:19:08 +0600

    task 23_2 donegit status!

--- week5 - strings/task23_2.py
+++ week5 - strings/task23_2.py
@@ -1,15 +1,29 @@
 s = input('enter a word to encrypt: ')
-fstStr = s[0:fLen]
-sndStr = 0
-thdStr = 0
+fLen = 0
+sLen = 0
+thLen = 0
 decryptStr = ""
 keyLen = 0
 if len(s) % 3 == 0:
     fLen = len(s) // 3
-    snLen = len(s) // 3
-    thLen = len(s) // 3
+    sLen = (len(s) // 3) + fLen
+    thLen = (len(s) // 3) + sLen
 elif (len(s) % 3) == 2:
     fLen = (len(s) // 3) + 1
-    snLen = (len(s) // 3) + 1
+    sLen = ((len(s) // 3) + 1) + fLen
+    thLen = len(s)-1
 elif (len(s) % 3) == 1:
     fLen = (len(s) // 3) + 1
+    sLen = (len(s) // 3) + fLen
+    thLen = len(s)
+
+fstStr = s[0:fLen]
+sndStr = s[fLen:sLen]
+thdStr = s[sLen:thLen+1]
+
+for i in range(len(fstStr)):
+    decryptStr += fstStr[i]
+    decryptStr += sndStr[i]
+    if i < len(thdStr):
+        decryptStr += thdStr[i]
+print(decryptStr)
