cc39e1e12a1b35fb5733c6830cf4d171cd850c64
elemanjan <mr.eleman@gmail.com>
2020-12-14 19:11:45 +0600

    task 38 donegit add .!

---
+++ week5 - strings/while/task2.py
@@ -0,0 +1,7 @@
+s = input('enter: ')
+vow = 'aioeyu'
+i = 0
+while i < len(vow):
+    if vow[i] in s:
+        print('hello world!')
+    i += 1
---
+++ week5 - strings/while/task3.py
@@ -0,0 +1,5 @@
+s = input('enter text: ')
+i = 0
+while i < len(s):
+    print(s[i], end=' ')
+    i += 2
