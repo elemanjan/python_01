eaf5e4026190b14fe4e0b828b315f42ede1258e7
elemanjan <namele93@gmail.com>
2020-12-14 13:09:04 +0600

    task 38 done

--- week5 - strings/task38.py
+++ week5 - strings/task38.py
@@ -2,12 +2,16 @@ s = input('enter a sequence: ')
 # s = s.lower()
 w = "bob"
 count = 0
+i = 0
 i = len(w)
 j = 0
 while i < len(s):
     s2 = s[j:i]
     if s2 == w:
         count += 1
+        i += 1
+        j += 1
+    else:
         i += 1
         j += 1
 print(count)
